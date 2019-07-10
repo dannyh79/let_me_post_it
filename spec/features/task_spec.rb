9a9f5dcbe952a8b16522de7c1c6728b0a0794d2e
danny-ch han <chenghsuan.han@gmail.com>
2019-07-10 12:01:44 +0800

    Modified the feature spec of model "Task"

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -4,18 +4,40 @@ RSpec.describe Task, type: :feature do
   let(:title) { Faker::Lorem.sentence }
   let(:description) { Faker::Lorem.paragraph }
 
-  describe 'index' do
+  describe 'visit the index of tasks' do
     it do
+      3.times do
+        create(:task)
+      end
       visit tasks_path
-      expect(page).to have_css('h1', text: "#{I18n.t("tasks.heading.index")}")
-      expect(page).to have_css('a', text: "#{I18n.t("tasks.table.create_a_new_task")}")
-      expect(page).to have_css('table th', text: "#{I18n.t("tasks.table.title")}")
-      expect(page).to have_css('table th', text: "#{I18n.t("tasks.table.start_time")}")
-      expect(page).to have_css('table th', text: "#{I18n.t("tasks.table.end_time")}")
-      expect(page).to have_css('table th', text: "#{I18n.t("tasks.table.priority")}")
-      expect(page).to have_css('table th', text: "#{I18n.t("tasks.table.status")}")
-      expect(page).to have_css('table th', text: "#{I18n.t("tasks.table.description")}")
-      expect(page).to have_css('table th', text: "#{I18n.t("tasks.table.edit")}")
+
+      # test if the data entries are shown in index page
+      expect(page).to have_css(
+                                'table tbody tr:first-child td:first-child', 
+                                text: "#{Task.first.title}"
+                              )
+      expect(page).to have_css(
+                                'table tbody tr:first-child td:nth-child(6)', 
+                                text: "#{Task.first.description}"
+                              )
+      
+      expect(page).to have_css(
+                                'table tbody tr:nth-child(2) td:first-child', 
+                                text: "#{Task.second.title}"
+                              )
+      expect(page).to have_css(
+                                'table tbody tr:nth-child(2) td:nth-child(6)', 
+                                text: "#{Task.second.description}"
+                              )
+      
+      expect(page).to have_css(
+                                'table tbody tr:nth-child(3) td:first-child', 
+                                text: "#{Task.third.title}"
+                              )
+      expect(page).to have_css(
+                                'table tbody tr:nth-child(3) td:nth-child(6)', 
+                                text: "#{Task.third.description}"
+                              )
     end
   end
 
