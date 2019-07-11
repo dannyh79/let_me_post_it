d2366dd74d10f3b28da7ad603ef4d4196e4173c1
Danny Cheng-Hsuan Han <30727089+dannyh79@users.noreply.github.com>
2019-07-11 16:09:16 +0800

    Fixed the migration file that could cause "PG::NotNullViolation" error while migrating (#16)

--- db/migrate/20190711035103_add_default_status_to_tasks.rb
+++ db/migrate/20190711035103_add_default_status_to_tasks.rb
@@ -1,9 +1,11 @@
 class AddDefaultStatusToTasks < ActiveRecord::Migration[5.2]
   def up
-    change_column :tasks, :status, :integer, null: false, default: 0
+    change_column_default :tasks, :status, 0
+    change_column_null :tasks, :status, false, 0
   end
   
   def down
-    change_column :tasks, :status, :integer
+    change_column_default :tasks, :status, nil
+    change_column_null :tasks, :status, true
   end
 end
