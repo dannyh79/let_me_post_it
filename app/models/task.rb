ca8d18f056ec93604a0bef5a59361811f0e341d3
danny-ch han <chenghsuan.han@gmail.com>
2019-07-11 14:52:38 +0800

    Table "tasks": changed the setting of column "priority" and added it as index on tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   enum status: [:pending, :ongoing, :done]
+  enum priority: [:low, :mid, :high]
 
   scope :created_at_asc, -> { order(created_at: :asc) }
   scope :created_at_desc, -> { order(created_at: :desc) }
